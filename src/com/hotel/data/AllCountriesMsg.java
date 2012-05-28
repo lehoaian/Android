eaba9fce58ecbd64a87ac23e4b36abd085903ba0
Ân Lê <lehoaian.it@gmail.com>
2012-05-28 22:55:58 +0700

    Add handler

---
+++ src/com/hotel/data/AllCountriesMsg.java
@@ -0,0 +1,15 @@
+package com.hotel.data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hoaian
+ * Date: 5/28/12
+ * Time: 10:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AllCountriesMsg {
+
+    public AllCountriesMsg() {
+
+    }
+}
---
+++ src/com/hotel/handler/AllCountriesHandler.java
@@ -0,0 +1,14 @@
+package com.hotel.handler;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hoaian
+ * Date: 5/28/12
+ * Time: 10:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AllCountriesHandler extends DefaultHandler {
+
+}
