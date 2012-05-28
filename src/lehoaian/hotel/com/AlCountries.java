1658648bdca8d7bda25d5560d38192c91dc17ce5
Ân Lê (FSOFT PC) <lehoaian.it@gmail.com>
2012-05-28 16:40:18 +0900

    Add contrustor

---
+++ src/lehoaian/hotel/com/AlCountries.java
@@ -0,0 +1,12 @@
+package lehoaian.hotel.com;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LETHANHH
+ * Date: 5/28/12
+ * Time: 3:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlCountries extends APIObject {
+
+}
