0dd87877682d52711ed4a812fb173ab8cf4c8de2
Ân Lê <lehoaian.it@gmail.com>
2012-05-29 01:09:27 +0700

    Create JUnit Test

--- src/com/hotel/core/CountriesController.java
+++ src/com/hotel/core/CountriesController.java
@@ -1,6 +1,14 @@
 package com.hotel.core;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+import com.hotel.activity.R;
 import com.hotel.data.Country;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import java.util.List;
 
 /**
@@ -12,12 +20,30 @@ import java.util.List;
  */
 public class CountriesController {
     List<Country> _listCountries;
+    DefaultHttpClient _client;
+    HttpGet _request;
+    HttpResponse _response;
+
+    CountriesController() {
+
+    }
 
     public List<Country> getAllCountries(){
+        //System.out(String.valueOf(R.string.all_countries_api));
+
+        _request = new HttpGet(Resources.getSystem().getString(R.string.all_countries_api));
+        //Log.d("HttpRequest", R.string.all_countries_api);
+        //_response = _client.execute(_request);
+        // _response.getParams();
 
         return _listCountries;
     }
     public Country getCountry(String countryCode){
         return new Country();
+    }
+
+    public int makeTest(){
+
+        return 1;
     }
 }
---
+++ src/com/hotel/core/CountriesControllerTest.java
@@ -0,0 +1,31 @@
+package com.hotel.core;
+
+import android.util.Log;
+import org.junit.Assert;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hoaian
+ * Date: 5/29/12
+ * Time: 12:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CountriesControllerTest {
+    CountriesController _contrl = new CountriesController();
+
+    @org.junit.Test
+    public void testGetAllCountries() throws Exception {
+        _contrl.getAllCountries();
+        Assert.assertEquals(17, 17);
+    }
+
+    @org.junit.Test
+    public void testGetCountry() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testMakeTest() throws Exception {
+        Assert.assertEquals(17, 17);
+    }
+}
