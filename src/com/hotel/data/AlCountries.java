967b7762e84888f1ab0c0dc071a596eedc7049d6
Ân Lê (FSOFT PC) <lehoaian.it@gmail.com>
2012-05-28 20:13:35 +0900

    Add contrustor

---
+++ src/com/hotel/data/AlCountries.java
@@ -0,0 +1,16 @@
+package com.hotel.data;
+
+import com.hotel.data.APIObject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: LETHANHH
+ * Date: 5/28/12
+ * Time: 3:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlCountries extends APIObject {
+    public AlCountries(API_TYPE type) {
+        super(type);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
